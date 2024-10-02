47ff753495267e6c18a8a250b0615094c3e0a31d
tolgabal <tolgabal752@gmail.com>
2024-10-02 16:01:09 +0300

    commit for tolgaDevs

--- android/app/src/main/kotlin/gastroblue/checkapp/demo1/gastrobluecheckapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package gastroblue.checkapp.demo1.gastrobluecheckapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/data/entity/hotpresentationasset.dart
+++
@@ -1,8 +0,0 @@
-class HotPresentationAsset{
-  int id;
-  String asset_name;
-  bool reuse_isactive;
-
-  HotPresentationAsset({required this.id, required this.asset_name, required this.reuse_isactive});
-
-}
